bbc693961597caa88851b3bb2f471280305bca98
Unknown <wfuentes1@mail.csuchico.edu>
2017-04-06 12:02:35 -0700

    finish file

--- ghostwriter.js
+++ ghostwriter.js
@@ -5,12 +5,13 @@ function writeTweet()
   var intro = ["Where other men ", "Where others are ", "We work in the dark ", "We are "]
   var body =["Nothing is True", "Everything is Permitted", "to serve the light", "Assassins", "to live the consequences"];
   var end = ["The creed is a warning", "Nothing is Forbidden"];
-   randIndex = randomUpTo(intro.length);
+   randIndex = randomUpTo(intro.length -1);
    sentence = intro[randIndex];
-   randIndex = randomUpTo(body.length);
-   sentence = intro + " " + body[randIndex];
-   randIndex = randomUpTo(end.length);
-   sentence = inro + " " + body + " " + end[randIndex];
+   randIndex = randomUpTo(body.length - 1);
+   sentence = sentence + " " + body[randIndex];
+   randIndex = randomUpTo(end.length - 1);
+   sentence = sentence + " " + end[randIndex];
+   document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
